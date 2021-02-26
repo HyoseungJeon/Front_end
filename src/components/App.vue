52ad80c451a4cc135b41260f86bc5e93e72ff677
Hyoseung Jeon <qmflwj1441@gmail.com>
2021-02-26 17:13:16 +0900

    Update Components

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
+    <header/>
+    <body/>
+    <footer/>
   </div>
 </template>
 
 <script>
+import Body from './Body.vue'
+import Footer from './Footer.vue'
+import Header from './Header.vue'
 
 export default {
   name: 'App',
   components: {
+    Header,
+    Body,
+    Footer,
   }
 }
 </script>
