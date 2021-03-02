1e139fecb09583ab74ed4c338d3c372bbcbe5bbb
Hyoseung Jeon <qmflwj1441@gmail.com>
2021-03-02 11:34:56 +0900

    Add base code

---
+++ src/views/EmployeeListView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:'EmployeeListView', 
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HeaderView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:'HeaderView', 
+}
+</script>
+
+<style>
+
+</style>
