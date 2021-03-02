1cf28807bc070cc43f6f83578d2f4c2aaf7e4f70
Hyoseung Jeon <qmflwj1441@gmail.com>
2021-03-02 13:57:46 +0900

    Fix main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import {App} from '~/components/'
 import VueRouter from 'vue-router'
-import SuiVue from 'semantic-ui-vue';
+import SuiVue from 'semantic-ui-vue'
+import 'semantic-ui-css/semantic.min.css';
 import router from './router/'
 import store from './store/'
 
