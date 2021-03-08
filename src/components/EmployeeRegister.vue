751abee319bc799a334e814226bd3b4bc0e5421f
yumi <youmi321@naver.com>
2021-03-08 11:21:07 +0900

    Fix EmployeeRegister to add validation check

--- src/components/EmployeeRegister.vue
+++ src/components/EmployeeRegister.vue
@@ -1,18 +1,33 @@
 <template>
   <div>
-      <employee-register-header-view/>
+      <ValidationObserver v-slot="{ handleSubmit }"> 
+      <form @submit.prevent="handleSubmit(onSubmit)">
+      <employee-register-header-view :employeeRegister="handleSubmit"/>
       <router-view id="EmployeeRegisterRouter"/>
+      </form>
+      </ValidationObserver>
   </div>
 </template>
 
 <script>
 import EmployeeRegisterHeaderView from '../views/EmployeeReigsterHeaderView'
+import {mapActions} from 'vuex'
+import {ValidationObserver} from 'vee-validate'
 
 export default {
   name: 'EmployeeRegister',
   components: {
       EmployeeRegisterHeaderView,
-  }
+      ValidationObserver,
+  },
+  methods : {
+    ...mapActions(['register']),
+    onSubmit : function(){
+      //API 
+      //this.register();
+      alert('등록!');
+    }
+  },
 }
 </script>
 
