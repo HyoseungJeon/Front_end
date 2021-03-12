80655b2246ac3990a5beddda2f75a6a8f3387fc7
Hyoseung Jeon <qmflwj1441@gmail.com>
2021-03-12 14:38:51 +0900

    Add CommCodeListDto

---
+++ src/model/dto/CommonCodeListDto.js
@@ -0,0 +1,7 @@
+class CommonCodeListDto{
+    constructor(){
+        this.group = [];
+    }
+}
+
+export default CommonCodeListDto
--- src/model/dto/index.js
+++ src/model/dto/index.js
@@ -1,2 +1,3 @@
 export {default as EmployeeListDto} from './EmployeeListDto'
 export {default as EmployeeSearchDto} from './EmployeeSearchDto'
+export {default as CommonCodeListDto} from './CommonCodeListDto'
