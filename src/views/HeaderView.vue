1e9be18ebaa543dc7e173b16ffc9c92f771600c4
Hyoseung Jeon <qmflwj1441@gmail.com>
2021-03-14 18:29:03 +0900

    Update homeBtn from HeaderView

--- src/views/HeaderView.vue
+++ src/views/HeaderView.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <img id="logo_form" src='@/assets/images/plateer_logo.png' width="200" height="50"/>
+    <img id="logo_form" src='@/assets/images/plateer_logo.png' width="200" height="50"
+    @click="onClickHomeBtn"/>
   </div>
 </template>
 
 <script>
 export default {
     name:'HeaderView', 
+    methods:{
+      onClickHomeBtn:function(){
+        this.$router.push({name : 'home'})
+      },
+    }
 }
 </script>
 
