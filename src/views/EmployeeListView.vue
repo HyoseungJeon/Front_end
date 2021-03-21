501794362451f98ebbc049e6583e835883156507
yumi_park <yumip1997@gmail.com>
2021-03-22 02:50:11 +0900

    Fix EmployeeListView to add sort

--- src/views/EmployeeListView.vue
+++ src/views/EmployeeListView.vue
@@ -1,11 +1,20 @@
 <template>
-  <div id="employee_list_form">
+  <div id="employee_list_form" :style="employeeListForm">
     <sui-table selectable="selectable" celled="celled">
-    <sui-table-header id="employee-list-header-form">
-        <sui-table-row>
-            <sui-table-header-cell>이름</sui-table-header-cell>
-            <sui-table-header-cell>부서</sui-table-header-cell>
-            <sui-table-header-cell>직급</sui-table-header-cell>
+    <sui-table-header>
+        <sui-table-row id="employee-list-header-form">
+            <sui-table-header-cell>
+              <sui-icon name="sort up" @click="onClickSort('name', 'up')" />
+              <sui-icon name="sort down" @click="onClickSort('name', 'down')"/>
+              이름</sui-table-header-cell>
+            <sui-table-header-cell>
+              <sui-icon name="sort up" @click="onClickSort('department', 'up')" />
+              <sui-icon name="sort down" @click="onClickSort('department', 'down')"/>
+              부서</sui-table-header-cell>
+            <sui-table-header-cell>
+              <sui-icon name="sort up" @click="onClickSort('position', 'up')" />
+              <sui-icon name="sort down" @click="onClickSort('position', 'down')"/>
+              직급</sui-table-header-cell>
         </sui-table-row>
     </sui-table-header>
     <sui-table-body>
@@ -24,6 +33,8 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import { DepartmentSortUtil, PositionSortUtil } from '@/util/sortUtils'
+
 export default {
     name:'EmployeeListView',
     data:function(){
@@ -32,19 +43,58 @@ export default {
           top : '0px',
         },
         selectedIndex : null,
+        arraySort : require('array-sort'),
       }
+    },
+    created(){
+      window.addEventListener('scroll', this.handlerScroll);
+    },
+    destroyed(){
+      window.removeEventListener('scroll', this.handlerScroll)
     },
     methods : {
       ...mapActions(['employeeFind']),
         onClickEmployeeRow:function(employeeId, index){
-          this.selectedIndex = index;
-        this.employeeFind(employeeId)
-        .then(
+        this.selectedIndex = index;
+        this.employeeFind(employeeId).then(
           status => status === 200 ? this.$router.push('EmployeeInfoRetrieveView') : ''
         )
-        .catch(error => {
-          console.log(error);
+        .catch(error => { console.log(error);
         })
+      },
+      onClickSort : function(option, order){
+        switch(option){
+          case 'name' : {
+            if(order === 'up'){
+              this.arraySort(this.employeeList, 'name');
+            }else{
+              this.arraySort(this.employeeList, 'name', {reverse : true});
+            }
+            break;
+          }
+          case  'department' : {
+            if(order === 'up'){
+              this.arraySort(this.employeeList, DepartmentSortUtil.compareDept('department'));
+            }else{
+              this.arraySort(this.employeeList, DepartmentSortUtil.compareDept('department'), {reverse: true});
+            }
+            break;
+          }
+          case 'position' : {
+            if(order === 'up'){
+              this.arraySort(this.employeeList, PositionSortUtil.comparePosition('position'));
+            }else{
+              this.arraySort(this.employeeList, PositionSortUtil.comparePosition('position'), {reverse: true});
+            }
+            break;
+          }
+
+        }
+        
+
+      },
+      handlerScroll(){
+        this.employeeListForm.top = window.scrollY + "px";
       },
     },
     computed:{
@@ -61,12 +111,12 @@ export default {
     width: 250px;
     position: relative;
     height: auto;
+    padding-left: 10px;
     transition: 500ms;
     top:0px
   }
   #employee-list-header-form{
     text-align: center;
-    height: 43px;
   }
   .employee-list-row-select{
     background: #e8e8e8;
